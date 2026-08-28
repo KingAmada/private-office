ffcd159a00aaeb25daa09b209472cfda9142d339
KingAmada <sarkiamada@gmail.com>
2026-08-28 09:51:51 +0100

    Add Private Office service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='private-office-static-v2';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).origin!==location.origin)return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(x=>x.put(e.request,copy));return r}))) });
