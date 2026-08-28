fafadc9863810c6fe8740c2cf40898984d046372
KingAmada <sarkiamada@gmail.com>
2026-08-28 09:49:41 +0100

    Add static runtime configuration

---
+++ config.js
@@ -0,0 +1,8 @@
+window.PRIVATE_OFFICE_CONFIG = {
+  APP_NAME: 'Private Office',
+  GOOGLE_CLIENT_ID: 'PASTE_YOUR_GOOGLE_OAUTH_WEB_CLIENT_ID.apps.googleusercontent.com',
+  AI_GATEWAY_URL: 'https://YOUR-WORKER.YOUR-SUBDOMAIN.workers.dev',
+  ROOT_FOLDER: 'Private Office',
+  MAX_AI_FILE_MB: 8,
+  SYNC_BATCH_SIZE: 8
+};
