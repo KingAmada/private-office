b5a59b81b53042dfa6d713cc860e4d6495b8146b
KingAmada <sarkiamada@gmail.com>
2026-08-28 15:42:39 +0100

    Refresh PWA cache for repaired indexing build

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='private-office-static-v2';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./manifest.webmanifest'];
+const CACHE='private-office-static-v3';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./manifest.webmanifest','./favicon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).origin!==location.origin)return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(x=>x.put(e.request,copy));return r}))) });
